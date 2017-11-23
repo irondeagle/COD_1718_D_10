a99c24d779e2d44a5b4a39a9235366a3e399471d
irondeagle <30833805+irondeagle@users.noreply.github.com>
2017-11-23 13:00:08 +0100

    phaser loaded

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,6 @@
-import {loadImageInCatalog} from './functions/lib.js';
-import Player from './classes/Player.js';
-import Keyboard from './classes/Keyboard.js';
-import Vector from './classes/Vector.js';
-import Bullet from './classes/Bullet.js';
-import Bird from './classes/Bird.js';
-import CollisionDetector from './classes/CollisionDetector.js';
-import Explosion from './classes/Explosion.js';
-import Platform from './classes/Platform.js';
-
 {
   let canvas;
   let ctx;
-  let player;
-  let keyboard;
-  let bird;
-  let platform;
-  let bulletBirdDetector;
-  let playerBirdDetector;
-  let gameOver = false;
-  let score = 0;
-  let health = 3;
 
 
   const init = () => {
